adabc55963079dd7b353206f51160907ffae990c
bunny0806 <bunnyfernandes0806@gmail.com>
2020-03-23 00:32:16 +0530

    create bsaic home page

--- ICHdetector/urls.py
+++ ICHdetector/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',views.home,name='home')
 ]
