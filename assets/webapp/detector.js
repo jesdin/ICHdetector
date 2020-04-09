1374d3c9de0bfdaea6c0bda5cb1b977dfad17daa
Jesdin Raphael <jesdinraphael@gmail.com>
2020-04-09 20:35:46 +0530

    set up csrf tokens

--- assets/webapp/detector.js
+++ assets/webapp/detector.js
@@ -1,11 +1,20 @@
 $("#submit").on("click", function () {
   console.log(uppy.getFiles());
-  // var link = document.createElement('a');
-  // link.href = uppy.getFiles()[0]["preview"];
-  // link.download = uppy.getFiles()[0]["name"];
-  // document.body.appendChild(link);
-  // link.click();
-  // document.body.removeChild(link);
+
+  csrftoken = $('input[name=csrfmiddlewaretoken]').val();
+
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
+
   if (uppy.getFiles()['length'] > 0) {
     $.ajax({
       type: 'POST',
@@ -13,7 +22,6 @@ $("#submit").on("click", function () {
       data: {
         image: uppy.getFiles()[0]["preview"],
         name: uppy.getFiles()[0]["name"],
-        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
         action: 'post'
       },
       success: function (json) {
--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+import matplotlib.pyplot as plt
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
+@csrf_protect
 def go_to_detector(request):
     return render(request,'detector.html')
 
+@csrf_protect
 def checkICH(request):
-    print("Ajax Complete")
+    print(request.POST.get("name"))
     return render(request,'detector.html')
-    # return render(request, )
